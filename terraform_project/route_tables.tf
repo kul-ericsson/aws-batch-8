e9c73e3be1c971b64a80d08bf5b7ac27f9f2e501
Kulbhushan <kulbhushan.mayer@>
2021-02-01 13:27:46 +0530

    created Route Table for Nat Gatewau Route

--- terraform_project/route_tables.tf
+++ terraform_project/route_tables.tf
@@ -8,3 +8,14 @@ resource "aws_route_table" "igw_rt" {
     gateway_id = aws_internet_gateway.igw.id
   }
 }
+
+resource "aws_route_table" "natgw_rt" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    "Name" = "thinknyx-${var.tagName}-natgw"
+  }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.natgw.id
+  }
+}
