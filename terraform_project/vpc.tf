3f779dc3eb65a7d06b2e4e973096fbf16d83c5f3
Kulbhushan <kulbhushan.mayer@>
2021-02-01 15:37:41 +0530

    updated to take first CIDR_BLOCK digit as input

--- terraform_project/vpc.tf
+++ terraform_project/vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "vpc" {
-  cidr_block = "10.10.0.0/16"
+  cidr_block = "${var.cidr_block}.10.0.0/16"
   instance_tenancy = "default"
   enable_dns_support = true
   enable_dns_hostnames = true
