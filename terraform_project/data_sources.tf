8b8e69b2e8326a7eb3aacf58dcc79e1df689ddf8
Kulbhushan <kulbhushan.mayer@>
2021-02-01 14:01:12 +0530

    updated to fetch route table details for default VPC

--- terraform_project/data_sources.tf
+++ terraform_project/data_sources.tf
@@ -6,3 +6,11 @@ data "aws_vpc" "default" {
     values = ["default"]
   }
 }
+
+data "aws_route_table" "default"{
+  vpc_id = data.aws_vpc.default.id
+  filter {
+    name = "tag:Name"
+    values = ["default"]
+  }
+}
