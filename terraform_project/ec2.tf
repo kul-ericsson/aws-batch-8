cafd9d665a6d86d6127cb845072af4adc225bc80
Kulbhushan <kulbhushan.mayer@>
2021-02-01 15:33:55 +0530

    created EC2 server

---
+++ terraform_project/ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "ec2" {
+  ami = "ami-0010499d417b154b6"
+  instance_type = "t2.micro"
+  key_name = "kul-ericsson-thinknyx"
+  tags = {
+    "Name" = "thinknyx-${var.tagName}"
+  }
+  availability_zone = "us-east-2b"
+  vpc_security_group_ids = [ aws_security_group.sg.id ]
+}
