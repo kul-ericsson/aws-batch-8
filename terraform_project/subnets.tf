73b482475e740775866867821aca812d39616f9d
Kulbhushan <kulbhushan.mayer@>
2021-02-01 15:49:58 +0530

    updated to take first CIDR_BLOCK digit as input

--- terraform_project/subnets.tf
+++ terraform_project/subnets.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "sn-1" {
   vpc_id = aws_vpc.vpc.id
-  cidr_block = "10.10.10.0/24"
+  cidr_block = "${var.cidr_block}.10.10.0/24"
   availability_zone = "us-east-2a"
   tags = {
     "Name" = "thinknyx-${var.tagName}-public"
@@ -9,7 +9,7 @@ resource "aws_subnet" "sn-1" {
 }
 resource "aws_subnet" "sn-2" {
   vpc_id = aws_vpc.vpc.id
-  cidr_block = "10.10.20.0/24"
+  cidr_block = "${var.cidr_block}.10.20.0/24"
   availability_zone = "us-east-2b"
   tags = {
     "Name" = "thinknyx-${var.tagName}-private"
@@ -17,7 +17,7 @@ resource "aws_subnet" "sn-2" {
 }
 resource "aws_subnet" "sn-3" {
   vpc_id = aws_vpc.vpc.id
-  cidr_block = "10.10.30.0/24"
+  cidr_block = "${var.cidr_block}.10.30.0/24"
   availability_zone = "us-east-2c"
   tags = {
     "Name" = "thinknyx-${var.tagName}-private"
