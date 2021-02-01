97384e39691400cc6527e64946e85d34efad8805
Kulbhushan <kulbhushan.mayer@>
2021-02-01 13:29:38 +0530

    updated resource name

--- terraform_project/subnet_associations.tf
+++ terraform_project/subnet_associations.tf
@@ -2,11 +2,11 @@ resource "aws_route_table_association" "igw_rt" {
   subnet_id = aws_subnet.sn-1.id
   route_table_id = aws_route_table.igw_rt.id
 }
-resource "aws_route_table_association" "igw_rt" {
+resource "aws_route_table_association" "natgw_rt_2" {
   subnet_id = aws_subnet.sn-2.id
   route_table_id = aws_route_table.natgw_rt.id
 }
-resource "aws_route_table_association" "igw_rt" {
+resource "aws_route_table_association" "natgw_rt_3" {
   subnet_id = aws_subnet.sn-3.id
   route_table_id = aws_route_table.natgw_rt.id
 }
