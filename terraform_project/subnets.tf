1995c738f6dd2247896bb63314a1cc5be821d979
Kulbhushan <kulbhushan.mayer@>
2021-02-01 13:09:38 +0530

    updated to reflect as public & private subnets

--- terraform_project/subnets.tf
+++ terraform_project/subnets.tf
@@ -3,15 +3,16 @@ resource "aws_subnet" "sn-1" {
   cidr_block = "10.10.10.0/24"
   availability_zone = "us-east-2a"
   tags = {
-    "Name" = "thinknyx-${var.tagName}"
+    "Name" = "thinknyx-${var.tagName}-public"
   }
+  map_public_ip_on_launch = true
 }
 resource "aws_subnet" "sn-2" {
   vpc_id = aws_vpc.vpc.id
   cidr_block = "10.10.20.0/24"
   availability_zone = "us-east-2b"
   tags = {
-    "Name" = "thinknyx-${var.tagName}"
+    "Name" = "thinknyx-${var.tagName}-private"
   }
 }
 resource "aws_subnet" "sn-3" {
@@ -19,6 +20,6 @@ resource "aws_subnet" "sn-3" {
   cidr_block = "10.10.30.0/24"
   availability_zone = "us-east-2c"
   tags = {
-    "Name" = "thinknyx-${var.tagName}"
+    "Name" = "thinknyx-${var.tagName}-private"
   }
 }
