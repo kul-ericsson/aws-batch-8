e6af2dd6cfc5302f5743473eb92eebc98f4900bd
Kulbhushan <kulbhushan.mayer@>
2021-02-01 13:52:58 +0530

    updated to take values from data source

--- terraform_project/peering_connection.tf
+++ terraform_project/peering_connection.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc_peering_connection" "pc" {
   vpc_id = aws_vpc.vpc.id
   peer_owner_id = data.aws_caller_identity.current.account_id
-  peer_vpc_id = "value"
+  peer_vpc_id = data.aws_vpc.default.id
   tags = {
     "Name" = "thinknyx-${var.tagName}"
   }
