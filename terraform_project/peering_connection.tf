8bae762d32a240023da0f6fabb025182a3c48a3d
Kulbhushan <kulbhushan.mayer@>
2021-02-01 13:49:50 +0530

    created Peering Connection

---
+++ terraform_project/peering_connection.tf
@@ -0,0 +1,9 @@
+resource "aws_vpc_peering_connection" "pc" {
+  vpc_id = aws_vpc.vpc.id
+  peer_owner_id = data.aws_caller_identity.current.account_id
+  peer_vpc_id = "value"
+  tags = {
+    "Name" = "thinknyx-${var.tagName}"
+  }
+  auto_accept = true
+}
