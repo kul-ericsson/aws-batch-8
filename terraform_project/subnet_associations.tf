1c5d2acc39fbca18b9a2d9e638ca6c69f0204996
Kulbhushan <kulbhushan.mayer@>
2021-02-01 13:28:15 +0530

    created private subnets association with Nat GW Route Table

--- terraform_project/subnet_associations.tf
+++ terraform_project/subnet_associations.tf
@@ -2,3 +2,11 @@ resource "aws_route_table_association" "igw_rt" {
   subnet_id = aws_subnet.sn-1.id
   route_table_id = aws_route_table.igw_rt.id
 }
+resource "aws_route_table_association" "igw_rt" {
+  subnet_id = aws_subnet.sn-2.id
+  route_table_id = aws_route_table.natgw_rt.id
+}
+resource "aws_route_table_association" "igw_rt" {
+  subnet_id = aws_subnet.sn-3.id
+  route_table_id = aws_route_table.natgw_rt.id
+}
